72d5412c599a1fa1c7f91540230aef0868eb06c2
fbmusa97 <musasanay@gmail.com>
2021-05-09 23:19:59 +0200

    Design1.3

--- app/src/main/java/com/example/unirate30/UniRating.java
+++ app/src/main/java/com/example/unirate30/UniRating.java
@@ -1,13 +1,38 @@
 package com.example.unirate30;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class  UniRating extends AppCompatActivity {
 
+    private Button button1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uni_rating);
+
+        button1 = findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDegree();
+            }
+        });
+
+    }
+    // @Override
+    // public void onClick(View v) {
+    //   switch(v.getId()) {
+    //    case R.id.button:
+    //          openDegree();
+    //            break;
+    public void openDegree(){
+        Intent intent1 = new Intent(this, Degree.class);
+        startActivity(intent1);
     }
 }
+
