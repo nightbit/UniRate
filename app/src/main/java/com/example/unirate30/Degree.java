a40892fd01383889cb0b42f174358f97aa0be2f6
fbmusa97 <musasanay@gmail.com>
2021-05-10 17:37:42 +0200

    Design1.5

---
+++ app/src/main/java/com/example/unirate30/Degree.java
@@ -0,0 +1,33 @@
+package com.example.unirate30;
+
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+
+public class Degree extends AppCompatActivity {
+
+    TextInputLayout til_BsC;
+    AutoCompleteTextView act_BsC;
+
+    ArrayList<Button> buttonList_BsC;
+    ArrayAdapter<Button> buttonArrayAdapter_BsC;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_degree);
+
+        til_BsC = (TextInputLayout)findViewById(R.id.til_BsC);
+        act_BsC = (AutoCompleteTextView) findViewById(R.id.act_BsC);
+
+       buttonList_BsC = new ArrayList<>();
+        buttonList_BsC.add((Button)findViewById(R.id.button6));
+
+    }
+}
