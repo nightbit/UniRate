0b19171561389fb13fadbf1e21546c0009c16e9a
cantthinkofanam3 <81969918+cantthinkofanam3@users.noreply.github.com>
2021-06-14 22:02:02 +0200

    update

--- app/src/main/java/com/example/unirate30/BBEFÄCHER/SEMESTER2/kreko.java
+++ app/src/main/java/com/example/unirate30/BBEFÄCHER/SEMESTER2/kreko.java
@@ -2,9 +2,7 @@ package com.example.unirate30.BBEFÄCHER.SEMESTER2;
 
 import android.content.Intent;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.ImageButton;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.unirate30.R;
@@ -14,39 +12,65 @@ import java.util.ArrayList;
 
 public class kreko extends AppCompatActivity {
 
-    TextInputLayout til_Prof19;
-    AutoCompleteTextView act_Prof19;
+    Button krekobutton20;
+    RatingBar krekobar1;
+    RatingBar krekobar2;
+    RatingBar krekobar3;
 
-    ArrayList<String> ArrayList_Prof19;
-    ArrayAdapter<String> ArrayAdapter_Prof19;
+    TextInputLayout til_Prof9kreko;
+    AutoCompleteTextView act_Prof9kreko;
 
-    ImageButton nextButton22;
-    String selected_Prof19;
+    ArrayList<String> ArrayList_Prof9kreko;
+    ArrayAdapter<String> ArrayAdapter_Prof9kreko;
+
+    ImageButton krekonextButton12;
+    String selected_Prof9kreko;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kreko);
 
-        til_Prof19 = (TextInputLayout) findViewById(R.id.til_Prof19);
-        act_Prof19 = (AutoCompleteTextView) findViewById(R.id.act_Prof19);
+        krekobar1 = findViewById(R.id.krekoratingBar2);
+        krekobar1.setVisibility(View.INVISIBLE);
+        krekobar2 = findViewById(R.id.krekoratingBar4);
+        krekobar2.setVisibility(View.INVISIBLE);
+        krekobar3 = findViewById(R.id.krekoratingBar6);
+        krekobar3.setVisibility(View.INVISIBLE);
 
-        ArrayList_Prof19 = new ArrayList<>();
-        ArrayList_Prof19.add("Michael Wekerle-Dreier");
+
+        til_Prof9kreko = (TextInputLayout) findViewById(R.id.til_Prof9kreko);
+        act_Prof9kreko = (AutoCompleteTextView) findViewById(R.id.act_Prof9kreko);
+
+        ArrayList_Prof9kreko = new ArrayList<>();
+        ArrayList_Prof9kreko.add("Michael Wekerle-Dreier");
 
 
-        ArrayAdapter_Prof19 = new ArrayAdapter<>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, ArrayList_Prof19);
-        act_Prof19.setAdapter(ArrayAdapter_Prof19);
+        ArrayAdapter_Prof9kreko = new ArrayAdapter<>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, ArrayList_Prof9kreko);
+        act_Prof9kreko.setAdapter(ArrayAdapter_Prof9kreko);
 
-        act_Prof19.setThreshold(1);
+        act_Prof9kreko.setThreshold(1);
 
-        nextButton22 = findViewById(R.id.nextButton22);
-        nextButton22.setOnClickListener(new View.OnClickListener() {
+        krekobutton20 = findViewById(R.id.krekobutton6);
+        krekobutton20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (true){
+                    krekobar1.setVisibility(View.VISIBLE);
+                    krekobar2.setVisibility(View.VISIBLE);
+                    krekobar3.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
+
+        krekonextButton12 = findViewById(R.id.krekonextButton12);
+        krekonextButton12.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                selected_Prof19 = act_Prof19.getText().toString();
-                if (selected_Prof19.equals("Michael Wekerle-Dreier")) {
+                selected_Prof9kreko = act_Prof9kreko.getText().toString();
+                if (selected_Prof9kreko.equals("Michael Wekerle-Dreier")) {
                     opendreiner();
                 }
             }
